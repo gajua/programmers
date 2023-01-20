05f6f9a26c859a05a71b69aa9800e4c7922ad5c7
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-01-20 17:38:25 +0900

    [level 2] Title: 올바른 괄호, Time: 2.57 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/README.md
+++ 프로그래머스/lv2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 2.60 ms
+메모리: 36.9 MB, 시간: 2.57 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,12 @@
 function solution(s) {
   let count = 0;
-  
   for (let i = 0; i < s.length; i++) {
     s[i] === "(" ? count++ : count--;
-    if(count < 0){
-      return false
+    if (count < 0) {
+      return false;
     }
   }
-return count === 0 ? true : false
-
+    
+  return count === 0 ? true : false;
+    
 }
