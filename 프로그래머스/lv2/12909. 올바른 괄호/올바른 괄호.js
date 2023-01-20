7a4a508badd634e86f92131682736b6d27b99974
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-01-20 14:43:06 +0900

    [level 2] Title: 올바른 괄호, Time: 2.60 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,12 @@
 function solution(s) {
   let count = 0;
-  let answer =""
+  
   for (let i = 0; i < s.length; i++) {
     s[i] === "(" ? count++ : count--;
     if(count < 0){
       return false
     }
   }
-count===0 ? answer = true : answer = false
-  return answer
+return count === 0 ? true : false
+
 }
