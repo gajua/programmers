ccc58a8ff8e9816cdc8ec6aa7ad8ce7ac6511feb
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-01-25 17:04:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,20 +1,31 @@
 function solution(keyinput, board) {
-    let res = [0, 0];
-    for (let p of keyinput) {
-        switch (p) {
-            case "left":
-                if (-res[0] < board[0] / 2 - 1) res[0]--;
-                break;
-            case "right":
-                if (res[0] < board[0] / 2 - 1) res[0]++;
-                break;
-            case "up":
-                if (res[1] < board[1] / 2 - 1) res[1]++;
-                break;
-            case "down":
-                if (-res[1] < board[1] / 2 - 1) res[1]--;
-                break;
-        }
+  let x = 0;
+  let y = 0;
+  keyinput.map((i) => {
+    if (i === "left") {
+      x -= 1;
+    } else if (i === "right") {
+      x += 1;
+    } else if (i === "up") {
+      y += 1;
+    } else if (i === "down") {
+      y -= 1;
     }
-    return res;
+    if (Math.abs(x) > board[0] / 2) {
+      if (x < 0) {
+        x = -Math.floor(board[0] / 2);
+      } else {
+        x = Math.floor(board[0] / 2);
+      }
+    }
+    if (Math.abs(y) > board[1] / 2) {
+      if (y < 0) {
+        y = -Math.floor(board[1] / 2);
+      } else {
+        y = Math.floor(board[1] / 2);
+      }
+    }
+  });
+  return [x, y];
 }
+
