32db16decb7e243ca80a1fe141d81e7dd0f5a5b8
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-01-29 13:40:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+  var answer = 0;
+  for (let i = 0; i < 500; i++) {
+    if (num !== 1) {
+      if (num % 2 === 0) {
+        num = num / 2;
+        answer++;
+      } else if (num % 2 === 1) {
+        num = num * 3 + 1;
+        answer++;
+      }
+    } else if (num === 1) {
+      return answer;
+    }
+  }
+  return -1;
+}
