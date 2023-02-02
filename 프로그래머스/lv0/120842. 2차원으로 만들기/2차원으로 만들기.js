e5f71c1eea185c8024619d2846b984d540b6be14
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-02-03 04:14:25 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,9 +1,5 @@
 function solution(num_list, n) {
-  var answer = [];
-  for (let i = 0; i <= num_list.length + 5; i++) {
-    if (num_list.length !== 0) {
-      answer.push(num_list.splice(0, n));
-    }
-  }
-  return answer;
+  return Array(num_list.length / n)
+    .fill([])
+    .map((i) => num_list.splice(0, n));
 }
