c42a8b166eebf61e9b017548a15a398611b83353
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-02-07 01:24:28 +0900

    [level 1] Title: 소수 찾기, Time: 154.45 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,19 +1,16 @@
 function solution(n) {
-  // 2부터 n까지의 수로 구성된 Set
-  const s = new Set();
-  for (let i = 2; i <= n; i++) {
-    s.add(i);
-  }
-
-  // 2부터 n의 제곱근보다 작은 최대 정수까지 
-  for (let j = 2; j < Math.sqrt(n); j++) {
-    // Set에 해당 수가 포함되면
-    if (s.has(j)) {
-      // 그 수를 제외하고 그 수의 배수는 모두 삭제
-      for (let k = j * 2; k <= n; k += j) {
-        s.delete(k);
-      }
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
     }
-  }
-  return s.size;
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
 }
