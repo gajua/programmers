257aeb5b13872d595ea151e4dd2fac1f3d8f4670
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-02-11 23:09:30 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,22 +1,5 @@
 function solution(n, m) {
-  function gcd(a, b) {
-    const remainder = a % b;
-    if (remainder === 0) return b;
-    return gcd(b, remainder);
-  }
-  let getLCM = (num1, num2) => {
-    let lcm = 1;
-
-    while (true) {
-      if (lcm % num1 == 0 && lcm % num2 == 0) {
-        break;
-      }
-      lcm++;
-    }
-    return lcm;
-  };
-  let answer = [0, 0];
-  answer[0] = gcd(Math.max(n, m), Math.min(n, m));
-  answer[1] = getLCM(Math.max(n, m), Math.min(n, m));
-  return answer;
+  const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+  const lcm = (a, b) => a * b / gcd(a, b);
+  return [gcd(n, m), lcm(n, m)];
 }
