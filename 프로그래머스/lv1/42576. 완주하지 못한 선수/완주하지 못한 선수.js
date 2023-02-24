5871a8dd4b4ea03555cecbff99e8a96b3d7aeed1
sewon.kim <101968934+gajua@users.noreply.github.com>
2023-02-24 12:57:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.96 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,6 +2,8 @@ function solution(participant, completion) {
   participant.sort();
   completion.sort();
   for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) return participant[i];
+    if (completion[i] !== participant[i]) {
+      return participant[i];
+    }
   }
 }
